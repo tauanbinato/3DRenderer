0d55b3906b32a6b2bd7878e4bf9798c4ed6649d2
Tauan Binato <tauanflores@gmail.com>
2023-01-14 22:45:13 -0300

    Fixing make file to link sdl, making basic render loop to render a red window

--- src/main.c
+++ src/main.c
@@ -2,8 +2,9 @@
 #include <SDL2/SDL.h>
 #include <stdbool.h>
 
-SDL_Window * window;
-SDL_Renderer * renderer;
+bool is_running = false;
+SDL_Window * window = NULL;
+SDL_Renderer * renderer = NULL;
 
 bool initialize_window(void) {
 	if(SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -21,24 +22,65 @@ bool initialize_window(void) {
 	 );
 
 	 if (!window) {
-		fprinf(stderr, "Error creating SDL window. \n");
+		fprintf(stderr, "Error creating SDL window. \n");
 		return false;
 	 }
 
 	// Create a SDL Renderer
 	renderer = SDL_CreateRenderer(window, -1, 0);
 	if (!renderer) {
-		fprinf(stderr, "Error creating SDL renderer. \n");
+		fprintf(stderr, "Error creating SDL renderer. \n");
 		return false;
 	}
 
 	return true;
 }
 
-int main(void) {
-	/* Create a SDL Window */
+void setup(void){}
 
-	initialize_window();
+void process_input(void){
+	SDL_Event event;
+	SDL_PollEvent(&event);
+
+	switch (event.type)
+	{
+		case SDL_QUIT:
+			is_running = false;
+			break;
+
+		case SDL_KEYDOWN:
+			if (event.key.keysym.sym == SDLK_ESCAPE) {
+				is_running = false;
+			}
+            break;
+			
+		default:
+			break;
+	}
+}
+
+void update(void){}
+
+void render(void){
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+}
+
+int main(void) {
+
+	/* Create a SDL Window */
+	is_running = initialize_window();
+
+	setup();
+
+	while (is_running)
+	{
+		process_input();
+		update();
+		render();
+	}
+	
 
 	return 0;
 }
